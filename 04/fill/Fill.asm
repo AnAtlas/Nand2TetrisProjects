f5045dca06eca34da2ecc35a354b73dc042a997c
Derek Drost <rlwarrior32@hotmail.com>
2020-03-14 12:56:23 -0500

    Finished project4!

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,69 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+//R0 is the mode either drawing white(0) or black(0x7FFF)
+//R1 is the draw pointer
+
+@R0
+M=0
+
+(RESETDRAW)
+	@R1
+	M=0
+(LOOP)
+	@KBD
+	D=M
+	@R0
+	D=D-M
+	@SWITCHBLACK
+	D;JGT
+	@KBD
+	D=M
+	@R0
+	D=D-M
+	@DRAWWHITE
+	D;JEQ
+	@KBD
+	D=M
+	@DRAWBLACK
+	D;JGT
+(SWITCHWHITE)
+	@R0
+	M=0
+	@RESETDRAW
+	0; JMP
+(DRAWWHITE)
+	@SCREEN
+	D=A
+	@R1
+	D=D+M
+	A=D
+	M=0
+	@END
+	0; JMP
+(SWITCHBLACK)
+	@32767
+	D=A
+	@R0
+	M=D
+	@RESETDRAW
+	0; JMP
+(DRAWBLACK)
+	@SCREEN
+	D=A
+	@R1
+	D=D+M
+	A=D
+	M=-1
+(END)
+	@R1
+	M=M+1
+	D=M
+	@8192
+	D=D-A
+	@LOOP
+	D; JLT
+	@R1
+	M=0
+	@LOOP
+	0; JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,48 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@2
+M=0
+//For efficiency, make sure we loop n times, where n is the smaller of the two numbers
+@0
+D=M
+@1
+D=D-M
+@START
+D;JGT
+@0
+D=M
+@3
+M=D
+@1
+D=M
+@0
+M=D
+@3
+D=M
+@1
+M=D
+
+//R3 will be our running total
+(START)
+@3
+M=0
+(LOOP)
+    @1
+    D=M
+    @END
+    D;JEQ
+    @0
+    D=M
+    @3
+    D=D+M
+    M=D
+    @1
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @3
+    D=M
+    @2
+    M=D
